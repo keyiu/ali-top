f5d4a93389a853a1d9fc88e7dbb32aa73203f432
wuweijie <wu.weijie@outlook.com>
2020-04-14 10:19:25 +0800

    d

--- index.d.ts
+++ index.d.ts
@@ -1,7 +1,38 @@
 declare namespace aliTop {
-  declare const ApiClient;
-  declare const TmcClient;
-  declare const DingTalkClient;
+  const ApiClient: any;
+  const TmcClient: any;
+  const DingTalkClient: any;
+  interface Message {
+    ToUserName: string;
+    FromUserName: string;
+    MsgType: 'image' | 'voice' | 'shortvideo' | 'location' | 'link' | 'event';
+    CreateTime: number;
+    Event: 'subscribe' | 'subscribe'
+  }
+  interface TextMessage {
+    content: string;
+    type: 'text';
+  }
+  interface MusicMessage {
+    content: {
+      title: string,
+      description: string,
+      musicUrl: string,
+      hqMusicUrl: string,
+    };
+    type: 'music';
+  }
+  interface CustomerService {
+    kfAccount?: string;
+    type: 'customerService';
+  }
+  interface ImageTextMessage {
+    title: string;
+    description: string;
+    picurl: string;
+    url: string
+  }
+  const SendMessage: TextMessage | MusicMessage | CustomerService | ImageTextMessage
 }
 export = aliTop
  
